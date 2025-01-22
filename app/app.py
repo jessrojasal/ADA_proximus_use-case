5e0ec66bbeda567d60c9ccac72db56140a69dc62
Maxim Schuermans <maximschuermans@MacBook-Air-van-Maxim.local>
2025-01-22 15:55:42 +0100

    small bug in app.py

--- app/app.py
+++ app/app.py
@@ -73,7 +73,7 @@ def upload_file():
         flash(f"File '{filename}' uploaded successfully!")
         print("File uploaded successfully")
         
-        print(f'A file has been uploaded, the main loop will execute. Make sure that the key.json targets.csv files arer present! Tragets will be read from {targets_file} Emails will be written to {filename}')
+        #print(f'A file has been uploaded, the main loop will execute. Make sure that the key.json targets.csv files arer present! Tragets will be read from {./data/scraped_targets.csv'} Emails will be written to {filename}')
         scraper(csv_file = os.path.join(os.getcwd(), "data", "targets.csv"))
         process_csv_and_generate_emails(output_file='./data/output.csv', targets_file='./data/scraped_targets.csv', model_key='./key.json')
         create_campaigns(output_file='./data/output.csv')
