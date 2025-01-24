f5563f51423c89ababefca61b7cfdfe075d51bd7
Maxim Schuermans <maximschuermans@MacBook-Air-van-Maxim.local>
2025-01-24 08:32:16 +0100

    changed api endpoint to aws

--- gophish_engine/main.py
+++ gophish_engine/main.py
@@ -2,8 +2,8 @@ import csv
 from gophish import Gophish
 from gophish.models import Group, User, Template, Campaign
 
-API_KEY = "b953f2d2a428582b9457ff928ae612d8a640ce87b542ff59a584c4e7b7409180"
-BASE_URL = "http://94.110.206.175:3333/"  # Change port and URL as needed
+API_KEY = "de15a463fccdf2bcede8c18d31f1643c638c87932b3881efde9e5a795b06ad17"
+BASE_URL = "https://13.61.9.36:3333//"  # Change port and URL as needed
 api = Gophish(API_KEY, host=BASE_URL, verify=False)  # Set verify=True for SSL
 
 
