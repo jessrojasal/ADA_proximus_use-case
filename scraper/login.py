a2515d9d0071d95db8f092e6208585fe245c1314
Maxim Schuermans <maximschuermans@MacBook-Air-van-Maxim.local>
2025-01-24 08:51:39 +0100

    changed dashboard

--- scraper/login.py
+++ scraper/login.py
@@ -17,10 +17,10 @@ def launch_browser():
     """
     try:
         options = webdriver.ChromeOptions()
-        options.add_argument("--headless")
-        options.add_argument("--disable-gpu")
-        options.add_argument("--no-sandbox")
-        options.add_argument("--disable-dev-shm-usage")
+        #options.add_argument("--headless")
+        #options.add_argument("--disable-gpu")
+        #options.add_argument("--no-sandbox")
+        #options.add_argument("--disable-dev-shm-usage")
         
         #driver_service = ChromeService(ChromeDriverManager().install())
         print("Launching Chrome...")
