52e9d52b2955876b5455ab088a067196eaa60880
Jessica Rojas-Alvarado <jessra.93@gmail.com>
2025-04-15 11:01:03 +0200

    gitignore

--- archive/check_prompt.py
+++
@@ -1,4 +0,0 @@
-from email_generator.email_generation import process_csv_and_generate_emails
-
-output_file, targets_file, model_key = './data/output.csv', './data/targets.csv', './key.json'
-process_csv_and_generate_emails(output_file, targets_file, model_key)
